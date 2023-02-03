c78a90dd3e112c370d40157c3e4296bcb9ff94a9
Dalvan R. Rech <dalvanrrech@gmail.com>
2023-02-03 16:47:03 -0300

    ###Added - counter state - counter cubit
    ###Changed
    - counter cubit page
    - home page
    - main
    - pubspec.lock
    - pubspec.yaml

---
+++ lib/page_cubit/cubit/counter_state.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+part of 'counter_cubit.dart';
+
+abstract class CounterState extends Equatable {
+  final int counter;
+  const CounterState(this.counter);
+
+  @override
+  List<Object> get props => [counter];
+}
+
+//Estado inicial da tela
+class CounterStateInitial extends CounterState {
+  const CounterStateInitial() : super(0);
+}
+
+class CounterStateData extends CounterState {
+  const CounterStateData(int counter) : super(counter);
+}
